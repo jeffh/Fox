730e5a3745abf36644da1ca7d9a1ee54d8ac79da
Jeff Hui <jeff@jeffhui.net>
2014-11-30 16:56:07 -0800

    Complete Renaming FOX.h -> Fox.h
    Thanks case-insensitive file system.

---
+++ Fox/Public/Fox.h
@@ -0,0 +1,43 @@
+// protocols
+#import "FOXStateMachine.h"
+#import "FOXStateTransition.h"
+#import "FOXRandom.h"
+#import "FOXGenerator.h"
+
+// data structures
+#import "FOXSequence.h"
+#import "FOXRoseTree.h"
+#import "FOXRunnerResult.h"
+#import "FOXCommand.h"
+#import "FOXExecutedCommand.h"
+#import "FOXTransition.h"
+#import "FOXPropertyResult.h"
+
+// generators
+#import "FOXCoreGenerators.h"
+#import "FOXNumericGenerators.h"
+#import "FOXStringGenerators.h"
+#import "FOXSetGenerators.h"
+#import "FOXArrayGenerators.h"
+#import "FOXDictionaryGenerators.h"
+#import "FOXPropertyGenerators.h"
+#import "FOXStateMachineGenerators.h"
+#import "FOXGenericGenerators.h"
+
+// stateful testers
+#import "FOXFiniteStateMachine.h"
+
+// randomizers
+#import "FOXConstantRandom.h"
+#import "FOXDeterministicRandom.h"
+
+// runner
+#import "FOXRunner.h"
+
+// reporters
+#import "FOXStandardReporter.h"
+#import "FOXDebugReporter.h"
+
+// DSL
+#import "FOXMacros.h"
+#import "FOXDSL.h"
