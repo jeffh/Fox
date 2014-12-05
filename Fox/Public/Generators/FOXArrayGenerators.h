ad1bc5ceb710969266655e75cf6131a9ee978f89
Jeff Hui <jeff@jeffhui.net>
2014-12-05 00:15:26 -0800

    Added names to generators.

--- Fox/Public/Generators/FOXArrayGenerators.h
+++ Fox/Public/Generators/FOXArrayGenerators.h
@@ -5,6 +5,8 @@
 @protocol FOXSequence;
 
 
+/// Generates a fixed-size array where each element is in the same order
+/// as the specified sequence of generators.
 FOX_EXPORT id<FOXGenerator> FOXTupleOfGenerators(id<FOXSequence> generators);
 
 FOX_EXPORT id<FOXGenerator> FOXTuple(NSArray *generators);
--- Fox/Public/Generators/FOXNumericGenerators.m
+++ Fox/Public/Generators/FOXNumericGenerators.m
@@ -44,23 +44,23 @@ FOX_EXPORT id<FOXGenerator> FOXNonZeroInteger(void) {
 }
 
 FOX_EXPORT id<FOXGenerator> FOXFloat(void) {
-    return FOXGenMap(FOXDecimalNumber(), ^FOXRoseTree *(FOXRoseTree *generatedTree) {
+    return FOXWithName(@"Float", FOXGenMap(FOXDecimalNumber(), ^FOXRoseTree *(FOXRoseTree *generatedTree) {
         return [generatedTree treeByApplyingBlock:^id(NSDecimalNumber *element) {
             return @([element floatValue]);
         }];
-    });
+    }));
 }
 
 FOX_EXPORT id<FOXGenerator> FOXDouble(void) {
-    return FOXGenMap(FOXDecimalNumber(), ^FOXRoseTree *(FOXRoseTree *generatedTree) {
+    return FOXWithName(@"Double", FOXGenMap(FOXDecimalNumber(), ^FOXRoseTree *(FOXRoseTree *generatedTree) {
         return [generatedTree treeByApplyingBlock:^id(NSDecimalNumber *element) {
             return @([element doubleValue]);
         }];
-    });
+    }));
 }
 
 FOX_EXPORT id<FOXGenerator> FOXDecimalNumber(void) {
-    return FOXSized(^id<FOXGenerator>(NSUInteger size) {
+    return FOXWithName(@"DecimalNumber", FOXSized(^id<FOXGenerator>(NSUInteger size) {
         id<FOXGenerator> genShort = FOXChoose(@(MAX(-size, INT16_MIN)), @(MIN(size, INT16_MAX)));
         return FOXBind(genShort, ^id<FOXGenerator>(NSNumber *exponent) {
             return FOXBind(FOXChoose(@0, @(size)), ^id<FOXGenerator>(NSNumber *mantissa) {
@@ -74,5 +74,5 @@ FOX_EXPORT id<FOXGenerator> FOXDecimalNumber(void) {
                 });
             });
         });
-    });
+    }));
 }
