6e2e4b12783d7f26a5af019faa034818adaf82e7
Jeff Hui <jeff@jeffhui.net>
2015-01-12 01:44:44 -0800

    Added ability to download prebuilt LLVM from s3.

--- Foxling/LLVM/unpack.sh
+++ Foxling/LLVM/unpack.sh
@@ -8,9 +8,18 @@ set -e
 function msg {
     printf "$GREEN==>$CLEAR $@\n"
 }
+function submsg {
+    printf " $GREEN-->$CLEAR $@\n"
+}
 
 msg "Removing ${GREEN}llvm llvm_build${CLEAR}"
 rm -rf llvm llvm_build
+if [ ! -f llvm.tar.gz ]; then
+    URL=http://foxling-clang.jeffhui.net/llvm.tar.gz
+    msg "Downloading prebuilt LLVM & Clang binaries"
+    submsg "$URL as llvm.tar.gz"
+    curl -L $URL > llvm.tar.gz
+fi
 msg "Unpacking ${GREEN}llvm.tar.gz${CLEAR}"
 tar -zxvf llvm.tar.gz
 
