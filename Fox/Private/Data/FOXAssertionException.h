deb86251c5a30298707dae3df6b11c98c360b158
Jeff Hui <jeff@jeffhui.net>
2015-01-12 23:43:54 -0800

    Disable modules for Foxling. Apple Clang has a custom module implementation (to support Swift + ObjC interop).

---
+++ Fox/Private/Data/FOXAssertionException.h
@@ -0,0 +1,13 @@
+//
+//  FOXAssertionException.h
+//  Fox
+//
+//  Created by Jeff Hui on 1/12/15.
+//  Copyright (c) 2015 Jeff Hui. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FOXAssertionException : NSException
+
+@end
---
+++ Fox/Private/Data/FOXAssertionException.m
@@ -0,0 +1,13 @@
+//
+//  FOXAssertionException.m
+//  Fox
+//
+//  Created by Jeff Hui on 1/12/15.
+//  Copyright (c) 2015 Jeff Hui. All rights reserved.
+//
+
+#import "FOXAssertionException.h"
+
+@implementation FOXAssertionException
+
+@end
